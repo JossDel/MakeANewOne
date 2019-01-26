d5f461cf4a99d5e411e605aa60a31f8b75bc353e
Axel Thomsen <axelthomsen@hotmail.com>
2019-01-26 20:50:21 +0100

    Squirrel shooting, acorn prefab for shot

---
+++ WeCanMakeANewOne/Assets/_Scripts/SqrrrrrrrlShooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SqrrrrrrrlShooting : MonoBehaviour
+{
+    public int damagePerShot = 100;
+    public int ammo = 1;
+    //public float range = 100f;
+
+    //shot
+    public GameObject acornPrefab;
+    public Transform ShotSpawn;
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && ammo > 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        var acorn = (GameObject)Instantiate(acornPrefab, ShotSpawn.position, ShotSpawn.rotation);
+        acorn.GetComponent<Rigidbody>().velocity = acorn.transform.forward * 15;
+        Destroy(acorn, 2.0f);
+        ammo -= 1;
+
+    }
+
+}
