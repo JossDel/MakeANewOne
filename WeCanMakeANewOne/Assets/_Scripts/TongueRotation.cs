7f536ee319a5feb5c56f073de456c979f1325441
Axel Thomsen <axelthomsen@hotmail.com>
2019-01-27 16:19:32 +0100

    Frog lick

---
+++ WeCanMakeANewOne/Assets/_Scripts/TongueRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueRotation : MonoBehaviour
+{
+    Vector3 angle = new Vector3(0, 1, 0);
+
+    void Update()
+    {
+        transform.RotateAround(new Vector3(GameObject.FindGameObjectWithTag("FrogBody").transform.position.x, 0, GameObject.FindGameObjectWithTag("FrogBody").transform.position.z), angle, Time.deltaTime * 720);
+    }
+}
