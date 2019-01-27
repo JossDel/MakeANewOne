762157cffdc92af6f24c777fb09976b124e0ccc0
Unknown <joseph@delatolas.se>
2019-01-27 17:25:57 +0100

    Enemy spawnners

---
+++ WeCanMakeANewOne/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private float nextSpawnTime;
+
+    [SerializeField]
+    private GameObject Demon;
+    [SerializeField]
+    private float spawnDelay = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(Demon, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+}
